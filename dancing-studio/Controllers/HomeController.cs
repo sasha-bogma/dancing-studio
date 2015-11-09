39d9133acd54b42f79dd254e6c433009fcf51e29
Aleksandra <sasha.bogma@gmail.com>
2015-11-10 01:28:28 +0200

    Дни рождения на главной (на текущий день и неделю вперёд)

--- dancing-studio/Controllers/HomeController.cs
+++ dancing-studio/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using dancing_studio.DAL;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,8 +12,35 @@ namespace dancing_studio.Controllers
     {
         StudioContext db = new StudioContext();
 
+        private static bool IsLikeToday(DateTime? d)
+        {
+            if (d == null) return false;
+            DateTime today = new DateTime(((DateTime)d).Year, DateTime.Today.Month, DateTime.Today.Day);
+            return d == today;
+        }
+
+        private static bool IsBiggerThenToday(DateTime? d)
+        {
+            if (d == null) return false;
+            DateTime today = new DateTime( ((DateTime)d).Year, DateTime.Today.Month, DateTime.Today.Day);
+            return d > today;
+        }
+
         public ActionResult Index()
         {
+            var a = db.Students.Where(x => x.Birthday.Value.Month == DateTime.Today.Month && x.Birthday.Value.Day == DateTime.Today.Day).OrderBy(x => x.Name).ToList();
+            ViewBag.TodayBirthdayStudents = a;
+
+            List<Student> b = new List<Student>();
+            DateTime currentDate = DateTime.Today.AddDays(1);
+            while (currentDate != DateTime.Today.AddDays(7))
+            {
+                var students = db.Students.Where(x => x.Birthday.Value.Month == currentDate.Month && x.Birthday.Value.Day == currentDate.Day).OrderBy(x => x.Name).ToList();
+                b.AddRange(students);
+                currentDate = currentDate.AddDays(1);
+            }
+            ViewBag.NearestBirthDays = b;
+
             return View();
         }
 
